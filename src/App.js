e7a6588211be0725ec005e3e4618f8657558bf73
Vansh Sood <emailofvansh@gmail.com>
2022-11-21 12:09:43 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import shiftkey from "./shiftkey-logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="row">
+          <img src={shiftkey} alt="" />
+          <img src={logo} alt="" />
+        </div>
+        <h1>
+          Say Hello to <span style={{ color: "#61dafb" }}>React</span>
+        </h1>
+        <p style={{ fontSize: 14 }}>Presented by Vansh Sood</p>
       </header>
     </div>
   );
---
+++ src/data/tasks.js
@@ -0,0 +1,26 @@
+const tasks = [
+  {
+    id: 1,
+    title: "Complete Lab 6",
+    course: "CSCI 2110",
+    dueDate: "20 November",
+  },
+  {
+    id: 2,
+    title: "Peer Review Assignment",
+    course: "ASSC 1800",
+    dueDate: "21 November",
+  },
+  {
+    id: 3,
+    title: "POTW",
+    course: "CSCI 2141",
+    dueDate: "22 November",
+  },
+  {
+    id: 4,
+    title: "Assignment 5",
+    course: "CSCI 2110",
+    dueDate: "28 November",
+  },
+];
